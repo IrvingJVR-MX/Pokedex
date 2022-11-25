47ce252f9e31c90be3d1acd19c68355491c7e14b
IrvingJVR-MX <106118394+IrvingJVR-MX@users.noreply.github.com>
2022-11-25 10:26:52 -0600

    delete unused file

--- app/src/main/java/com/example/pokedex/utils/BaseListViewHolder.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pokedex.utils
-
-import android.view.View
-import androidx.recyclerview.widget.RecyclerView
-
-abstract class BaseListViewHolder<T> (itemView: View): RecyclerView.ViewHolder(itemView) {
-    abstract fun bind(item: T, position: Int)
-}
