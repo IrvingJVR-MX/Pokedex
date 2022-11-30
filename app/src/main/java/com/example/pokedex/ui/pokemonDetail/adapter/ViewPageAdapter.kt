41e9bbeed90d442609f2e2e70787eaf13bc161ec
Irving Villanueva <irving.villanueva@jalasoft.com>
2022-11-30 11:42:00 -0600

    Design changes were made

--- app/src/main/java/com/example/pokedex/ui/pokemonDetail/adapter/ViewPageAdapter.kt
+++ app/src/main/java/com/example/pokedex/ui/pokemonDetail/adapter/ViewPageAdapter.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.pokedex.ui.pokemonDetail.fragments.AboutFragment.view.AboutFragment
-import com.example.pokedex.ui.pokemonDetail.fragments.EvolutionFragment.EvolutionFragment
+import com.example.pokedex.ui.pokemonDetail.fragments.EvolutionFragment.view.EvolutionFragment
 import com.example.pokedex.ui.pokemonDetail.fragments.StatFragment.view.StatFragment
 
-internal class ViewPageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle, pokemonName: String): FragmentStateAdapter(fragmentManager, lifecycle){
+internal class ViewPageAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle, pokemonName: String, pokemonColor: Int): FragmentStateAdapter(fragmentManager, lifecycle){
     private val _pokemonName = pokemonName
+    private val _pokemonColor = pokemonColor
 
     override fun getItemCount(): Int {
         return 3
@@ -18,10 +19,10 @@ internal class ViewPageAdapter(fragmentManager: FragmentManager, lifecycle: Life
     override fun createFragment(position: Int): Fragment {
        return when (position){
             0-> {
-                AboutFragment(_pokemonName)
+                AboutFragment(_pokemonName, _pokemonColor)
             }
             1-> {
-                StatFragment(_pokemonName)
+                StatFragment(_pokemonName, _pokemonColor)
             }
             2 -> {
                 EvolutionFragment()
--- app/src/main/java/com/example/pokedex/ui/pokemonDetail/fragments/StatFragment/view/StatFragment.kt
+++ app/src/main/java/com/example/pokedex/ui/pokemonDetail/fragments/StatFragment/view/StatFragment.kt
@@ -14,13 +14,13 @@ import com.graphqlapollo.PokemonStatsInfoQuery
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class StatFragment(name: String) : Fragment() {
+class StatFragment(name: String, color: Int) : Fragment() {
     private var _binding: FragmentStatBinding? = null
     private val binding get() = _binding!!
     private lateinit var pokemonStats: PokemonStatsInfoQuery.Pokemon
     private val viewModel: StatViewModel by viewModels()
     private var pokemonName: String = name
-
+    private var pokemonColor: Int = color
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,6 +33,7 @@ class StatFragment(name: String) : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         observeData()
         viewModel.queryPokemonStats(pokemonName)
+        binding.tvBaseStats.setTextColor(pokemonColor)
     }
 
     private fun observeData() {
@@ -61,31 +62,37 @@ class StatFragment(name: String) : Fragment() {
         val hpObj = pokemonStats.stats?.find { it?.stat?.name == "hp" }
         binding.tvPokemonHp.text = hpObj?.base_stat.toString()
         binding.pbPokemonHp.progress  = hpObj?.base_stat ?: 0
+        binding.pbPokemonHp.setIndicatorColor(pokemonColor)
         total += hpObj?.base_stat ?: 0
         //Attack
         val atkObj = pokemonStats.stats?.find { it?.stat?.name == "attack" }
         binding.tvPokemonAtk.text = atkObj?.base_stat.toString()
         binding.pbPokemonAtk.progress = atkObj?.base_stat ?:0
+        binding.pbPokemonAtk.setIndicatorColor(pokemonColor)
         total += atkObj?.base_stat ?:0
         //Defense
         val defObj = pokemonStats.stats?.find { it?.stat?.name == "defense" }
         binding.tvPokemonDef.text =  defObj?.base_stat.toString()
         binding.pbPokemonDef.progress = defObj?.base_stat ?:0
+        binding.pbPokemonDef.setIndicatorColor(pokemonColor)
         total += defObj?.base_stat ?:0
         //Special atk
         val spAtkObj = pokemonStats.stats?.find { it?.stat?.name == "special-attack" }
         binding.tvPokemonSpAtk.text = spAtkObj?.base_stat.toString()
         binding.pbPokemonSpAtk.progress = spAtkObj?.base_stat ?:0
+        binding.pbPokemonSpAtk.setIndicatorColor(pokemonColor)
         total += spAtkObj?.base_stat ?:0
         //Special def
         val spDefObj = pokemonStats.stats?.find { it?.stat?.name == "special-defense" }
         binding.tvPokemonSpDef.text = spDefObj?.base_stat.toString()
         binding.pbPokemonSpDef.progress =  spDefObj?.base_stat ?:0
+        binding.pbPokemonSpDef.setIndicatorColor(pokemonColor)
         total += spDefObj?.base_stat ?:0
         //Speed
         val speedObj = pokemonStats.stats?.find { it?.stat?.name == "speed" }
         binding.tvPokemonSpeed.text =  speedObj?.base_stat.toString()
         binding.pbPokemonSpeed.progress = speedObj?.base_stat ?:0
+        binding.pbPokemonSpeed.setIndicatorColor(pokemonColor)
         total += speedObj?.base_stat ?:0
         //Total
         binding.tvPokemonTotal.text = total.toString()
